c85a58d38e35dc722d60e760a543fe99d49911d6
mobad <mostafa.m.badawey@gmail.com>
2020-08-18 03:44:49 +0200

    [FEAT]: Add routes types.

---
+++ src/types/role.ts
@@ -0,0 +1,12 @@
+export enum UserRoles {
+  admin = 'Admin',
+  customer = 'Customer',
+  supplier = 'Supplier',
+}
+
+// export const userRoles = {
+//   admins: [String(UserRoles.admin)],
+//   customers: [String(UserRoles.customer)],
+//   suppliers: [String(UserRoles.supplier)],
+//   all: [String(UserRoles.admin), String(UserRoles.customer), String(UserRoles.supplier)],
+// };
---
+++ src/types/routes.ts
@@ -0,0 +1,10 @@
+export enum AuthRoutes {
+  dashboard = '/dashboard',
+  transactions = '/transactions',
+  products = '/products',
+}
+
+export enum NonAuthRoutes {
+  login = '/',
+  register = '/register',
+}
