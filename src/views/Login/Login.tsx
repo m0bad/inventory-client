841cb03f913936cb8d7740db30a68d76d1390373
mobad <mostafa.m.badawey@gmail.com>
2020-08-18 13:58:47 +0200

    [FEAT]: prevent authenticated users from entering auth pages.

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,18 +1,26 @@
 import { Col, Layout, Row } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { LoginPayload } from '../../types/user';
 import { loginUser } from '../../store/users/authSlice';
 import { AuthForm } from '../../components/auth';
+import { AuthAPi } from '../../api/users/auth';
 
 const { Content } = Layout;
+const authClient = new AuthAPi();
 
 export function Login() {
   const dispatch = useDispatch();
   const history = useHistory();
   // @ts-ignore
   const error = useSelector(state => state.auth.error);
+
+  useEffect(() => {
+    if (authClient.getToken()) {
+      history.push('/dashboard');
+    }
+  }, [history]);
 
   const onLogin = async (payload: LoginPayload) => {
     const result = await dispatch(loginUser(payload));
--- src/views/Register/Register.tsx
+++ src/views/Register/Register.tsx
@@ -1,18 +1,26 @@
 import { Col, Layout, Row } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { RegisterPayload } from '../../types/user';
 import { registerUser } from '../../store/users/authSlice';
 import { AuthForm } from '../../components/auth';
+import { AuthAPi } from '../../api/users/auth';
 
 const { Content } = Layout;
+const authClient = new AuthAPi();
 
 export function Register() {
   const dispatch = useDispatch();
   const history = useHistory();
   // @ts-ignore
   const error = useSelector(state => state.auth.error);
+
+  useEffect(() => {
+    if (authClient.getToken()) {
+      history.push('/dashboard');
+    }
+  }, [history]);
 
   const onRegister = async (payload: RegisterPayload) => {
     const result = await dispatch(registerUser(payload));
